9fc1355bc493cc06b1108c26d6b4891d204f57cc
Imdad Hossain <imdad.nsu@gmail.com>
2020-02-21 10:33:30 +1300

    commit

--- sumOfNumbers.java
+++ sumOfNumbers.java
@@ -4,18 +4,18 @@ public class sumOfNumbers
 	{
 		
 		int[] a={3,6,2,9,5};
-		int sum=0;
+		int sums=0;
 
 		for(int i=0;i<a.length;i++)
 		{
-			sum+=a[i];
+			sums+=a[i];
 		}
 		
 		multOfNumbers multOfNumbers=new multOfNumbers();
 		multOfNumbers.multiplyNumbers();
 		sum sum= new sum();
 		sum.sum();
-		System.out.println("The Sum of numbers are - " + sum);
+		System.out.println("The Sum of numbers are - " + sums);
 
 
 	}
